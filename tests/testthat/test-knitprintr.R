8cfdd3a1dd0866280ffb94b01e12c06792a84b29
Dieter Menne <dieter.menne@menne-biomed.de>
2016-11-30 16:16:24 +0100

    Used zip reference html file

--- tests/testthat/test-knitprintr.R
+++ tests/testthat/test-knitprintr.R
@@ -6,7 +6,10 @@ test_that("Knit rmd and compare with expected output",{
   use_rmd = "knitprinter.Rmd"
   expect_true(file.exists(use_rmd))
   rmarkdown::render(use_rmd, output_file = outfile, quiet = TRUE)
-  expect_html = "knitprinter.html"
+  expect_zip = "knitprinter.zip"
+  expect_true(file.exists(expect_zip))
+  unzip(expect_zip, exdir = tempdir())
+  expect_html = file.path(tempdir(), "knitprinter.html")
   expect_true(file.exists(expect_html))
   expect_true(all.equal(readLines(outfile), readLines(expect_html)))
 })
